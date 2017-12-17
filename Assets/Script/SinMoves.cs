ad0f1ce71e51e28a845b0dc80de6f6e05c15e530
yuan31810 <yuan31810@gmail.com>
2017-12-17 22:16:02 +0900

    ゆっくり浮いていくオブジェクトを取るゲーム
    オブジェクトの動きを実験してみました

---
+++ Assets/Script/SinMoves.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SinMoves : MonoBehaviour {
+    public float width = 0.2f;
+    UpMoves upMove;
+    public float flSpeed = 1.0f;
+    private Rigidbody2D myRigid2D;
+	// Use this for initialization
+	void Start () {
+        //myRigid2D = GetComponent<Rigidbody2D>();
+        upMove = GetComponent<UpMoves>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector2(Mathf.PingPong(Time.time,width), transform.position.y);
+        upMove.Move(transform.up * flSpeed);
+        
+    }
+    
+}
