15e921571d441aeeabd95f7efed7fc25d9157d80
yuan31810 <yuan31810@gmail.com>
2017-12-23 16:36:27 +0900

    浮いてくるオブジェクトを取るゲーム
    オブジェクトがランダムで生成されるようになりました

---
+++ Assets/Script/BubbleGenelator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleGenelator : MonoBehaviour {
+    
+    public GameObject bubbleItem;
+   
+    // Use this for initialization
+    IEnumerator Start () {
+        while (true)
+        {
+            Instantiate(bubbleItem, transform.position, transform.rotation);
+
+
+            yield return new WaitForSeconds(3.0f);
+        }
+        
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+    
+}
--- Assets/Script/SinMoves.cs
+++ Assets/Script/SinMoves.cs
@@ -7,15 +7,23 @@ public class SinMoves : MonoBehaviour {
     UpMoves upMove;
     public float flSpeed = 1.0f;
     private Rigidbody2D myRigid2D;
+    private float preX;
+    private float preY;
 	// Use this for initialization
 	void Start () {
         //myRigid2D = GetComponent<Rigidbody2D>();
         upMove = GetComponent<UpMoves>();
-	}
+
+        preX = Random.Range(-6, 6);
+        preY = Random.Range(-50, 0);
+        transform.position = new Vector2(preX, preY);
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        transform.position = new Vector2(Mathf.PingPong(Time.time,width), transform.position.y);
+
+       
+        transform.position = new Vector2(preX+Mathf.PingPong(Time.time,width), transform.position.y);
         upMove.Move(transform.up * flSpeed);
         
     }
